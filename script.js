24278d083a869fe60b684730888131d90ba638db
elmira1385 <elmirashokriyan64@gmail.com>
2025-09-22 02:14:07 +0330

    v1

---
+++ script.js
@@ -0,0 +1,9 @@
+const socialEl=document.getElementById("socialEl");
+const flashEl=document.getElementById("flashEl");
+const listEl=document.getElementById("listEl");
+
+socialEl.addEventListener("click",()=>{
+    listEl.classList.remove("showhidden")
+});
+
+
