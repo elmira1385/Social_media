98a0f25455c7991375312326c307ec6106141dc8
elmira1385 <elmirashokriyan64@gmail.com>
2025-09-22 19:52:08 +0330

    v4

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-const socialEl=document.getElementById("socialEl");
-const flashEl=document.getElementById("flashEl");
-const listEl=document.getElementById("listEl");
-const FacebookEl=document.getElementById("FacebookEl")
-const YouTubeEl=document.getElementById("YouTubeEl")
-const TwitterEl=document.getElementById("TwitterEl")
-const LinkedinEl=document.getElementById("LinkedinEl")
+const socialEl = document.getElementById("socialEl");
+const flashEl = document.getElementById("flashEl");
+const listEl = document.getElementById("listEl");
+const FacebookEl = document.getElementById("FacebookEl");
+const YouTubeEl = document.getElementById("YouTubeEl");
+const TwitterEl = document.getElementById("TwitterEl");
+const LinkedinEl = document.getElementById("LinkedinEl");
 
-const textSocial=document.getElementById("textSocial")
+const textSocial = document.getElementById("textSocial");
 
-socialEl.addEventListener("click",()=>{
-    listEl.classList.toggle("showhidden")
-    // flashEl.animate({transform:"rotateZ(3.142rad)"})
+socialEl.addEventListener("click", () => {
+  listEl.classList.toggle("showhidden");
+  flashEl.classList.toggle("routete");
 });
-FacebookEl.addEventListener("click",()=>{
-    socialEl.classList.remove("social")
-    textSocial.innerHTML=FacebookEl.innerHTML
-    listEl.classList.add("showhidden")
-
-})
-YouTubeEl.addEventListener("click",()=>{
-    socialEl.classList.remove("social")
-    textSocial.innerHTML=YouTubeEl.innerHTML
-    listEl.classList.add("showhidden")
-})
-TwitterEl.addEventListener("click",()=>{
-    socialEl.classList.remove("social")
-    textSocial.innerHTML=TwitterEl.innerHTML
-    listEl.classList.add("showhidden")
-})
-LinkedinEl.addEventListener("click",()=>{
-    socialEl.classList.remove("social")
-    textSocial.innerHTML=LinkedinEl.innerHTML
-    listEl.classList.add("showhidden")
-
-})
-
-
+FacebookEl.addEventListener("click", () => {
+  socialEl.classList.remove("social");
+  textSocial.innerHTML = FacebookEl.innerHTML;
+  listEl.classList.add("showhidden");
+});
+YouTubeEl.addEventListener("click", () => {
+  socialEl.classList.remove("social");
+  textSocial.innerHTML = YouTubeEl.innerHTML;
+  listEl.classList.add("showhidden");
+});
+TwitterEl.addEventListener("click", () => {
+  socialEl.classList.remove("social");
+  textSocial.innerHTML = TwitterEl.innerHTML;
+  listEl.classList.add("showhidden");
+});
+LinkedinEl.addEventListener("click", () => {
+  socialEl.classList.remove("social");
+  textSocial.innerHTML = LinkedinEl.innerHTML;
+  listEl.classList.add("showhidden");
+});
