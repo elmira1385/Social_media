8ac091a4bb2860ced6bd722c0b62783d0e3d73d8
elmira1385 <elmirashokriyan64@gmail.com>
2025-09-22 13:49:42 +0330

    v2

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const socialEl=document.getElementById("socialEl");
 const flashEl=document.getElementById("flashEl");
 const listEl=document.getElementById("listEl");
+const FacebookEl=document.getElementById("FacebookEl")
 
 socialEl.addEventListener("click",()=>{
-    listEl.classList.remove("showhidden")
+    listEl.classList.toggle("showhidden")
 });
+FacebookEl.addEventListener("click",()=>{
+    socialEl.classList.remove("social")
+    socialEl.textContent=FacebookEl.textContent
+})
 
 
